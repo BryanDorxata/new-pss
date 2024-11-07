38d6eb42a19d4d4f285c6702f9e2c8e84e134154
BryanDorxata <133320281+BryanDorxata@users.noreply.github.com>
2024-11-07 22:01:43 +0800

    Update route.js

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,4 +1,4 @@
-export async function GET(req) {
+export async function GET() {
   return new Response(JSON.stringify({ message: "Hello, world!" }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
